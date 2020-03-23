0fba95bc1dd856407331f261c72684b27de79f5a
sheepwall <a.sve@live.se>
2020-03-23 06:17:17 -0700

    Added save theme (only pywal JSON for now) More helpful README

--- README.md
+++ README.md
@@ -1,9 +1,10 @@
 Set XResources settings in a file, from different input types and run the new file.
 
-TODO
-DONE Take pywal json file
-DONE turn into pywal struct
-DONE turn into general struct
-DONE insert into template
-DONE Output xresources file
-Update xrdb with new file
+### Usage
+
+    theme help              Returns short help and usage
+    theme set filename      Set current colors to colors in "filename"
+    theme save filename     Save current colors to file "filename"
+
+### Filetypes
+Currently, only XResources files (content like: ``*.color0: #ABCDEF``) and "pywal" JSON files (found in [this repo](https://github.com/dylanaraps/pywal)) are supported. I will add more when I feel the need to.
--- theme.go
+++ theme.go
@@ -6,6 +6,8 @@ import (
     "io/ioutil"
     "os"
     "os/exec"
+    "regexp"
+    "strings"
     "text/template"
 )
 
@@ -40,16 +42,12 @@ type PywalJsonTheme struct {
     Colors PywalJsonColors `json:"colors"`
 }
 
-// XResources line
-type XREntry struct {
-    Id string
-    Value string
+// XResources theme
+type XRTheme struct {
+    Entries map[string]string
 }
 
-// XReasources theme (several lines)
-type XRTheme struct {
-    Entries []XREntry
-}
+const XRDB_FILENAME = "Xtheme"
 
 func check(e error) {
     if e != nil {
@@ -61,29 +59,91 @@ func main() {
     xdgConfig, err := os.UserConfigDir()
     check(err)
 
+    if (os.Args[1] == "help") {
+        print_help()
+    }
+
     if (len(os.Args) < 2) {
-        fmt.Println("Invalid usage: Supply a filename.")
+        fmt.Println("Invalid usage: Specify a command.")
         os.Exit(1)
     }
-    filename := os.Args[1]
-    fmt.Println("Reading file ", filename)
+
+    if (os.Args[1] == "set") {
+        if (len(os.Args) != 3) {
+            fmt.Println("Usage: theme set filename")
+            os.Exit(1)
+        }
+
+        filename := os.Args[2]
+        var theme = load(filename)
+        set(theme)
+    } else if (os.Args[1] == "save") {
+        if (len(os.Args) != 3) {
+            fmt.Println("Usage: theme save filename")
+            os.Exit(1)
+        }
+
+        filename := os.Args[2]
+        var theme = load(xdgConfig + "/" + XRDB_FILENAME)
+        for id, value := range theme.Entries {
+            fmt.Println(id, value)
+        }
+
+        save(theme, filename)
+    } else {
+        fmt.Println("Usage: theme set filename")
+        os.Exit(1)
+    }
+}
+
+func print_help() {
+    fmt.Println("Available commands: help, save, set")
+    fmt.Println("   Save current theme to file: theme save filename")
+    fmt.Println("   Load and set current theme from file: theme set filename")
+    fmt.Println("Example usage:")
+}
+
+func load(filename string) XRTheme {
+    if _, err := os.Stat(filename); err != nil {
+        fmt.Println("Error reading file. Does the file exist?")
+        os.Exit(1)
+    }
+
     filedata, err := ioutil.ReadFile(filename)
     check(err)
 
-    var pwTheme PywalJsonTheme
-    err = json.Unmarshal(filedata, &pwTheme)
+    var theme XRTheme
+    suffix := fileSuffix(filename)
+    if (suffix == "json") {
+        // Assumed pywal-type of json
+        var pwTheme PywalJsonTheme
+        err = json.Unmarshal(filedata, &pwTheme)
+        check(err)
+
+        theme = pwTheme.toXRTheme()
+    } else if (suffix == "") {
+        // Assumed XResources file
+        theme = theme.fromXRFile(strings.Split(string(filedata), "\n"))
+    } else {
+        fmt.Println("File format (suffix) not valid.")
+        os.Exit(1)
+    }
+
+    return theme
+}
+
+func set(theme XRTheme) {
+    xdgConfig, err := os.UserConfigDir()
     check(err)
 
-    theme := pwTheme.toXRTheme()
-
     tmpl, err := template.New("theme").Parse(
-`{{- range .Entries -}}
-{{.Id}}: {{.Value}}
+`{{- range $id, $value := .Entries -}}
+{{$id}}: {{$value}}
 {{end -}}`,
     )
     check(err)
 
-    themefile, err := os.Create(xdgConfig + "/Xtheme")
+    themefile, err := os.Create(xdgConfig + "/" + XRDB_FILENAME)
     check(err)
     defer themefile.Close()
 
@@ -92,37 +152,118 @@ func main() {
     xrdbCmd := exec.Cmd{
         Dir: xdgConfig,
         Path: "/usr/bin/xrdb",
-        Args: []string{"-override", "Xtheme"},
+        Args: []string{ "-override", XRDB_FILENAME },
     }
 
     err = xrdbCmd.Run()
     check(err)
 }
 
+func save(theme XRTheme, filename string) {
+    // Only pywal json for now
+    var pwTheme PywalJsonTheme
+    pwTheme = pwTheme.fromXRTheme(theme)
+    jsonData, err := json.Marshal(pwTheme)
+    check(err)
+
+    err = ioutil.WriteFile(filename, jsonData, 0644)
+    check(err)
+}
+
+func fileSuffix(filename string) string {
+    slash := strings.LastIndex(filename, "/")
+    if (slash >= 0) {
+        filename = filename[slash:]
+    }
+
+    dot := strings.LastIndex(filename, ".")
+    if dot >= 0 {
+        return filename[dot + 1:]
+    } else {
+        return ""
+    }
+}
+
+func (xrTheme XRTheme) fromXRFile(lines []string) XRTheme {
+    xrTheme.Entries = make(map[string]string)
+
+    i := 0
+    for _, line := range lines {
+        re_comment := regexp.MustCompile(`^\s*;`)
+        if (re_comment.Match([]byte(line))) {
+            continue
+        }
+
+        re_entry := regexp.MustCompile(
+            `(?P<Id>^[\w\d\*]+\.[\w\d\*\.]+):\s*` + // e.g "*.color1: "
+            `(?P<Value>[#\w\d\*\.]+\s*$)`,          // e.g "#EAF0B1"
+        )
+
+        submatches := re_entry.FindSubmatch([]byte(line))
+        if (len(submatches) == 3) {
+            // 3 values: [ full, id, value ]
+            xrTheme.Entries[string(submatches[1])] = string(submatches[2])
+            i++
+        }
+    }
+
+    return xrTheme
+}
+
 func (pwTheme PywalJsonTheme) toXRTheme() XRTheme {
     theme := XRTheme {
-        Entries: []XREntry {
-            {Id: "*.foreground", Value: pwTheme.Special.Foreground },
-            {Id: "*.background", Value: pwTheme.Special.Background },
-            {Id: "*.cursor", Value: pwTheme.Special.Cursor },
-            {Id: "*.color0", Value: pwTheme.Colors.Color0 },
-            {Id: "*.color1", Value: pwTheme.Colors.Color1 },
-            {Id: "*.color2", Value: pwTheme.Colors.Color2 },
-            {Id: "*.color3", Value: pwTheme.Colors.Color3 },
-            {Id: "*.color4", Value: pwTheme.Colors.Color4 },
-            {Id: "*.color5", Value: pwTheme.Colors.Color5 },
-            {Id: "*.color6", Value: pwTheme.Colors.Color6 },
-            {Id: "*.color7", Value: pwTheme.Colors.Color7 },
-            {Id: "*.color8", Value: pwTheme.Colors.Color8 },
-            {Id: "*.color9", Value: pwTheme.Colors.Color9 },
-            {Id: "*.color10", Value: pwTheme.Colors.Color10 },
-            {Id: "*.color11", Value: pwTheme.Colors.Color11 },
-            {Id: "*.color12", Value: pwTheme.Colors.Color12 },
-            {Id: "*.color13", Value: pwTheme.Colors.Color13 },
-            {Id: "*.color14", Value: pwTheme.Colors.Color14 },
-            {Id: "*.color15", Value: pwTheme.Colors.Color15 },
+        Entries: map[string]string {
+            "*.foreground": pwTheme.Special.Foreground,
+            "*.background": pwTheme.Special.Background,
+            "*.cursor": pwTheme.Special.Cursor,
+            "*.color0": pwTheme.Colors.Color0,
+            "*.color1": pwTheme.Colors.Color1,
+            "*.color2": pwTheme.Colors.Color2,
+            "*.color3": pwTheme.Colors.Color3,
+            "*.color4": pwTheme.Colors.Color4,
+            "*.color5": pwTheme.Colors.Color5,
+            "*.color6": pwTheme.Colors.Color6,
+            "*.color7": pwTheme.Colors.Color7,
+            "*.color8": pwTheme.Colors.Color8,
+            "*.color9": pwTheme.Colors.Color9,
+            "*.color10": pwTheme.Colors.Color10,
+            "*.color11": pwTheme.Colors.Color11,
+            "*.color12": pwTheme.Colors.Color12,
+            "*.color13": pwTheme.Colors.Color13,
+            "*.color14": pwTheme.Colors.Color14,
+            "*.color15": pwTheme.Colors.Color15,
         },
     }
 
     return theme
+}
+
+func (pwTheme PywalJsonTheme) fromXRTheme(xrTheme XRTheme) PywalJsonTheme {
+    pwTheme = PywalJsonTheme {
+        Special: PywalJsonSpecial {
+            Foreground: xrTheme.Entries["*.foreground"],
+            Background: xrTheme.Entries["*.background"],
+            Cursor: xrTheme.Entries["*.cursor"],
+        },
+        Colors: PywalJsonColors {
+            Color0: xrTheme.Entries["*.color0"],
+            Color1: xrTheme.Entries["*.color1"],
+            Color2: xrTheme.Entries["*.color2"],
+            Color3: xrTheme.Entries["*.color3"],
+            Color4: xrTheme.Entries["*.color4"],
+            Color5: xrTheme.Entries["*.color5"],
+            Color6: xrTheme.Entries["*.color6"],
+            Color7: xrTheme.Entries["*.color7"],
+            Color8: xrTheme.Entries["*.color8"],
+            Color9: xrTheme.Entries["*.color9"],
+            Color10: xrTheme.Entries["*.color10"],
+            Color11: xrTheme.Entries["*.color11"],
+            Color12: xrTheme.Entries["*.color12"],
+            Color13: xrTheme.Entries["*.color13"],
+            Color14: xrTheme.Entries["*.color14"],
+            Color15: xrTheme.Entries["*.color15"],
+        },
+    }
+
+    return pwTheme
 }
